e101c44a662f1a4192d6e4b6643b20f5bcb25d17
jhoyosg5 <33663056+jhoyosg5@users.noreply.github.com>
2018-07-28 14:27:00 -0500

    organizando el archivo de la BD

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,37 @@
-module.exports = {
-    'url': 'mongodb://localhost/proyecto1'
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'test';
+
+var config = {
+  development: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'Proyecto'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/proyecto1-dev'
+  },
+
+  test: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'Proyecto'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo-server/proyecto1-test'
+  },
+
+  production: {
+    baseUrl: "/",
+    root: rootPath,
+    app: {
+      name: 'Proyecto'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://mongo-server/proyecto1-prod'
+  }
 };
+
+module.exports = config[env];
